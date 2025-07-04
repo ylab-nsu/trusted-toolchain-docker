b508c1c0a85dc2f8b36f3c5fe4146f1b409f311d
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-07-05 00:04:14 +0700

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+docker rm buildroot_rootfs || true
+
 ./make-docker-cfg.sh &&
   docker build -t buildroot-build . &&
   docker run --name "buildroot_rootfs" buildroot-build &&
+  rm -rf ./output &&
   mkdir -p output &&
   docker cp buildroot_rootfs:/output/rootfs.tar ./output/ && true
